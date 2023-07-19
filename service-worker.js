8cddc56f12890cc9b01d53d0eefdffb9a8b2e1d0
Nicole Orbe <nicoleorbe@outlook.com>
2023-07-18 20:53:42 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -36,20 +36,3 @@ self.addEventListener("beforeinstallprompt", (event) => {
   // For example:
   // showAddToHomeScreenBanner();
 });
-
-// self.addEventListener("fetch", (event) => {
-//   event.respondWith(
-//     fetch(event.request)
-//       .then((response) => {
-//         // Do any additional processing on the response if needed
-//         return response;
-//       })
-//       .catch((error) => {
-//         console.error("Error fetching:", error);
-//         // Optionally, you can return a custom response for failed fetch requests
-//         return new Response("An error occurred while fetching.", {
-//           status: 500,
-//         });
-//       })
-//   );
-// });
